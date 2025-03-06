28f4cd9ac70964245abeb33b23174e9b3daa7525
zpnever <143313019+zpnever@users.noreply.github.com>
2025-03-06 15:19:14 +0700

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,13 +11,8 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ['next'],
-    rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
-    },
+    extends: ['next/core-web-vitals', 'next/typescript'],
   }),
 ]
-
 
 export default eslintConfig;
